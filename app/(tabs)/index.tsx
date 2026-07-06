faecde4f3b039fb8d77910090ce9df048f26aa32
Samuel Obayomi <obayomisamuel941@gmail.com>
2026-07-06 21:30:54 +0100

    feat:custom-fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,14 +9,14 @@ export default function App() {
   
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
+       <Text className="text-3xl font-sans-extrabold text-success">
         Welcome to Nativewind!
       </Text>
-      <Link href="/onboarding" className='mt-4 bg-primary rounded-[100px] text-white p-4'>Onboarding</Link>
-            <Link href="/(auth)/sign-in" className='mt-4 bg-primary rounded-[100px] text-white p-4'>Sign In</Link>
-                  <Link href="/(auth)/sign-up" className='mt-4 bg-primary rounded-[100px] text-white p-4'>Sign Up</Link>
-                  <Link href='/subscriptions/spotify' className='mt-4 bg-primary rounded-[100px] text-white p-4'>Spotify</Link>
-                  <Link href={{
+      <Link href="/onboarding" className='mt-4 bg-primary rounded-[100px] font-sans-bold text-white p-4'>Onboarding</Link>
+            <Link href="/(auth)/sign-in" className='mt-4 bg-primary rounded-[100px] font-sans-bold text-white p-4'>Sign In</Link>
+                  <Link href="/(auth)/sign-up" className='mt-4 bg-primary rounded-[100px] font-sans-bold text-white p-4'>Sign Up</Link>
+                  <Link href='/subscriptions/spotify' className='mt-4 bg-primary rounded-[100px] font-sans-bold text-white p-4'>Spotify</Link>
+                  <Link className='mt-4' href={{
                     pathname:'/subscriptions/[id]',
                     params:{id:'claude-max'}
                   }}>Claude Max Subscription</Link>
